c07cec34d8d7796fa23b458dee5c1ee7aa84fc9d
talron23 <108025960+talron23@users.noreply.github.com>
2023-11-01 18:26:14 +0200

    Delete daily-horoscope-app directory

--- daily-horoscope-app/Ansible-Terraform-Python-Project/README.md
+++
@@ -1,2 +0,0 @@
-# Ansible-Terraform-Python-Project
-This project leverages Terraform and Ansible for infrastructure automation, Nginx as a web server, Flask and Python for application development, and deploys on AWS.
--- daily-horoscope-app/application-code/nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export IP_WEB=$(cat /home/ubuntu/hosts.ini)
-sed -i 's#server_name;#server_name '"${IP_WEB}"';#' /home/ubuntu/application-code/nginx_config.j2
-sed -i 's#/0.0.0.0#/'"${IP_WEB}"'#' /home/ubuntu/application-code/app.py
-sed -i 's#127.0.0.1#'"${IP_WEB}"'#' /home/ubuntu/application-code/templates/index.html
